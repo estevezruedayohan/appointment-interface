74fd1a224c45138a346ceeebee49c70f685df243
Yohan Estevez <estevezruedayohan@gmail.com>
2026-01-25 00:15:59 +1100

    Creating DropDown component

---
+++ src/components/DropDown.jsx
@@ -0,0 +1,48 @@
+import { BiCheck } from "react-icons/bi";
+
+export default function DropDown() {
+  return (
+    <div
+      className="absolute right-0 mt-2 w-56 origin-top-right
+      rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-50"
+    >
+      <div
+        className="py-1"
+        role="menu"
+        aria-orientation="vertical"
+        aria-labelledby="options-menu"
+      >
+        <div
+          className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+          role="menuitem"
+        >
+          Pet Name <BiCheck />{" "}
+        </div>
+        <div
+          className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+          role="menuitem"
+        >
+          Owner Name <BiCheck />
+        </div>
+        <div
+          className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+          role="menuitem"
+        >
+          Date <BiCheck />
+        </div>
+        <div
+          className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer border-gray-1 border-t-2"
+          role="menuitem"
+        >
+          Asc <BiCheck />
+        </div>
+        <div
+          className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+          role="menuitem"
+        >
+          Desc <BiCheck />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,24 +1,46 @@
-import { HiSearch, HiChevronDown } from "react-icons/hi";
-
-const DropDown = () => {};
+import { BiSearch, BiCaretDown } from "react-icons/bi";
+import DropDown from "./DropDown";
+import { useState } from "react";
 
 export default function Search() {
+  const [toggle, setToggle] = useState(false);
   return (
-    <div>
-      <div className="flex items-center w-full max-w-md mx-auto">
-        <div className="flex w-full overflow-hidden border border-zinc-700 bg-stone-100 rounded-xl focus-within:ring-2 focus-within:ring-sky-500">
-          <div className="flex items-center justify-center pl-4 text-zinc-400">
-            <HiSearch size={20} />
-          </div>
-          <input
-            type="text"
-            placeholder="Search..."
-            className="w-full px-4 py-3 bg-transparent text-white outline-none placeholder:text-zinc-500"
-          />
-          <button className="flex items-center gap-1 px-4 py-3 border-l border-zinc-700 bg-sky-800 text-neutral-100 hover:bg-zinc-700 transition-colors">
-            <span>Filters</span>
-            <HiChevronDown />
+    <div className="mt-5 flex items-center w-full max-w-md mx-auto">
+      <div className="flex w-full border border-zinc-700 bg-neutral-200 rounded-xl focus-within:ring-2 focus-within:ring-sky-500">
+        <div className="flex items-center justify-center pl-4 text-zinc-400">
+          <BiSearch size={20} />
+          <label htmlFor="query" className="sr-only" />
+        </div>
+        <input
+          type="text"
+          name="query"
+          id="query"
+          className="w-full rounded-l-xl px-4 py-3 bg-transparent text-white outline-none placeholder:text-zinc-500"
+          placeholder="Search"
+        />
+        <div className="relative">
+          <button
+            onClick={() => setToggle(!toggle)}
+            type="button"
+            className="flex items-center whitespace-nowrap rounded-r-xl gap-1 px-4 py-3 border-l border-zinc-700 bg-blue-700/50 text-zinc-300 hover:bg-blue-700 transition-colors"
+            id="options-menu"
+            aria-haspopup="true"
+            aria-expanded="true"
+          >
+            <span>Sort By</span>
+            <BiCaretDown className="ml-2" />
           </button>
+          {toggle && (
+            <>
+              <div
+                className="fixed inset-0 z-40"
+                onClick={() => setToggle(false)}
+              ></div>
+              <div className="relative z-50">
+                <DropDown />
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
