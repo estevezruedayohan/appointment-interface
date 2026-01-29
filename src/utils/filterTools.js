76bad7522b8a1901d0345e8c1fe34f56dc12966f
Yohan Estevez <estevezruedayohan@gmail.com>
2026-01-29 21:57:50 +1100

    Adding ordering method and date handling in filterTool.js

--- src/utils/filterTools.js
+++ src/utils/filterTools.js
@@ -1,8 +1,23 @@
-export const filterAppointments = (list, searchTerm) => {
+export const filterAppointments = (list, searchTerm, sortBy, orderBy) => {
   
   return list.filter((item) => 
     item.petName.toLowerCase().includes(searchTerm.toLowerCase()) ||
     item.ownerName.toLowerCase().includes(searchTerm.toLowerCase()) ||
     item.aptNotes.toLowerCase().includes(searchTerm.toLowerCase())
-  )
+  ).sort((a, b) => {
+    let order = orderBy === "asc" ? 1 : -1
+
+    let aValue = a[sortBy]
+    let bValue = b[sortBy]
+
+    if(sortBy === 'aptDate'){
+      aValue = new Date(aValue).getTime()
+      bValue = new Date(bValue).getTime()
+    } else{
+      aValue = aValue.toLowerCase()
+      bValue = bValue.toLowerCase()
+    }
+
+    return aValue < bValue ? -1 * order : 1 * order
+  })
 }
